9807790b1ee1e3762b4672cb0e10629a87bac647
ColeANiblett <coleaniblett@gmail.com>
2022-06-30 22:37:27 -0700

    add About section

---
+++ src/components/About/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './About.css';
+import Saikal from '../../Saikal.svg';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div className="About">
+                <h1>About</h1>
+                <img src={Saikal} className='Saikal-photo' alt='Saikal'/>
+                <p>
+                    Abdymomunova Saikal is a  17 year old digital artist 
+                    living in Bishkek, Kyrhyzstan, where she is an active 
+                    member of the Dunkcomics publishing team.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default About;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,16 @@ import logo from '../../logo.svg';
 import './App.css';
 import Gallery from '../Gallery/Gallery';
 import Hero from '../Hero/Hero';
+import Sidebar from '../Sidebar/Sidebar';
+import About from '../About/About';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Sidebar />
       <Gallery />
+      <About />
     </div>
   );
 }
